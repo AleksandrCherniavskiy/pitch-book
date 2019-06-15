0ac98320d0d13a5ead50956adb99e315385fd139
AleksandrCherniavskiy <aleksandr.cherniavskyi28@gmail.com>
2019-06-15 21:52:30 +0300

    Added featuure logic.

--- js/features-1.js
+++ js/features-1.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
+$(function(){
+  $("button.feature-1__button_tab").on("click", function() {
+    if( !$(this).hasClass("feature-1__button_tab-active" )) {
+      $("button.feature-1__button_tab-active").removeClass("feature-1__button_tab-active");
+      $(this).addClass("feature-1__button_tab-active");
 
-  // jQuery methods go here...
-
+      let title = $(this).text();
+      $(".feature-1__title").text(title + ' title');
+    }
+  })
 });
