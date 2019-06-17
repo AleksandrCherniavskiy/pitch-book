a690eb6d9788c0bea94a986d7327537ad3240a51
AleksandrCherniavskiy <aleksandr.cherniavskyi28@gmail.com>
2019-06-17 22:49:16 +0300

    Fixed typo in variable name.

--- js/parallax.js
+++ js/parallax.js
@@ -3,10 +3,10 @@ $(function () {
     let scrollTop = $(this).scrollTop();
     let parallaxStart = 4300;
     let parallaxEnd = 4600;
-    let coeficientParallax = 0.76;
+    let coefficientParallax = 0.76;
 
     if (scrollTop >= parallaxStart && scrollTop <= parallaxEnd) {
-      let backgroundPosition = ((parallaxStart - scrollTop) * coeficientParallax).toFixed(2);
+      let backgroundPosition = ((parallaxStart - scrollTop) * coefficientParallax).toFixed(2);
 
       $(".parallax").css({backgroundPositionY: backgroundPosition + "px"});
     }
