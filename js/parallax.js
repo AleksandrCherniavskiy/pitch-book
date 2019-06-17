854c3cc55d30f3f880ba862bbdd8f39a37442a5b
AleksandrCherniavskiy <aleksandr.cherniavskyi28@gmail.com>
2019-06-17 20:32:19 +0300

    Added variable coeficientParallax.

--- js/parallax.js
+++ js/parallax.js
@@ -3,9 +3,10 @@ $(function () {
     let scrollTop = $(this).scrollTop();
     let parallaxStart = 4300;
     let parallaxEnd = 4600;
+    let coeficientParallax = 0.76;
 
     if (scrollTop >= parallaxStart && scrollTop <= parallaxEnd) {
-      let backgroundPosition = ((parallaxStart - scrollTop) * 0.76).toFixed(2);
+      let backgroundPosition = ((parallaxStart - scrollTop) * coeficientParallax).toFixed(2);
 
       $(".parallax").css({backgroundPositionY: backgroundPosition + "px"});
     }
